9c16d4e8e87735306e01a3205411106583d54a79
joshuashea <joshshea44@gmail.com>
2023-09-21 13:57:23 -0700

    added comments describing lines of code

--- src/models/clothes.js
+++ src/models/clothes.js
@@ -1,15 +1,22 @@
 'use strict';
 
+// This page creates a table-structured 'blueprint' that created data models - aka model.
 
-const Clothes = (sequelize, DataTypes) => sequelize.define('Clothes', {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  type: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-});
-
+// creates a model using Sequelize named 'Clothes'
+const Clothes = (sequelize, DataTypes) =>
+  sequelize.define('Clothes', {
+    // create a column in the 'Clothes' table
+    name: {
+      // the datatype of the values associated with this column
+      type: DataTypes.STRING,
+      // this means the name column is required, it cannot be omitted
+      allowNull: false,
+    },
+    type: {
+      type: DataTypes.STRING,
+      // in this instance, the type column is not required
+      allowNull: true,
+    },
+  });
+// exports Clothes so it can be used elsewhere
 module.exports = Clothes;
--- src/models/index.js
+++ src/models/index.js
@@ -1,15 +1,27 @@
 'use strict';
 
+// This page is a 'barrel roll' file, here's a description from ChatGPT:
+
+// "This code sets up a Sequelize connection to a database, creates instances
+// of the Food and Clothes models using the provided Sequelize instance and DataTypes,
+// and exports them along with the Sequelize instance for use in other parts of the
+// application."
+
+// These three lines import modules from Sequelize, and import Food and Clothes models
 const { Sequelize, DataTypes } = require('sequelize');
 const Food = require('./food.js');
 const Clothes = require('./clothes.js');
-
-const SQL_CONNECTION_STRING = process.env.SQL_CONNECTION_STRING || 'sqlite:memory:';
-
-const sequelize = new Sequelize(SQL_CONNECTION_STRING); 
-
+// configures SQL connection string to use a variable from a .env file or a sqlite database
+const SQL_CONNECTION_STRING =
+  process.env.SQL_CONNECTION_STRING || 'sqlite:memory:';
+// creates a Sequelize instance that is connecting to the database
+const sequelize = new Sequelize(SQL_CONNECTION_STRING);
+// exports the Sequelize instance as well as the Food and Clothes models
 module.exports = {
   sequelize,
+  // the Food and Clothes models are both passing the same arguments: sequelize, which was
+  // instantiated on line 18, and DataTypes, which is an object from Sequelize that the model uses
+  // to define the datatype of a column
   FoodModel: Food(sequelize, DataTypes),
   ClothesModel: Clothes(sequelize, DataTypes),
 };
